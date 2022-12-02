fe6f340b4add66cec296610fddb150ec5bff67bf
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-12-02 17:22:56 +0100

    backend done

--- index.js
+++ index.js
@@ -1,39 +1,103 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // Data
-const ServiceData = require("./Data/ServiceData.json");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.post("/", (req, res) => {
-  const size = req.body.sizelimit;
-  let PartialService = ServiceData.slice(0, size);
-  if (size <= 3) {
-    return res.send(PartialService);
-  } else {
-    PartialService = ServiceData.slice(0, size);
-    return res.send(PartialService);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n9cwtsk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function connection() {
+  try {
+    const servicesCollection = client
+      .db("HelixDatabase")
+      .collection("Services");
+    const ReviewCollection = client.db("HelixDatabase").collection("Myreviews");
+
+    app.post("/", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const cursorArray = await cursor.toArray();
+
+      const size = req.body.sizelimit;
+      let PartialService = cursorArray.slice(0, size);
+      if (size <= 3) {
+        return res.send(PartialService);
+      } else {
+        PartialService = cursorArray.slice(0, size);
+
+        return res.send(PartialService);
+      }
+    });
+
+    app.get("/details/:id", async (req, res) => {
+      const dataId = req.params.id;
+      const query = { _id: ObjectId(dataId) };
+      const data = await servicesCollection.findOne(query);
+      console.log(data);
+
+      res.send({ data });
+    });
+
+    app.post("/newReview", async (req, res) => {
+      const bdy = req.body.review;
+      // console.log(bdy);
+
+      const result = await ReviewCollection.insertOne(bdy);
+
+      res.send(result);
+    });
+
+    app.get("/oldReview", async (req, res) => {
+      const query = {};
+      const cursor = ReviewCollection.find(query);
+      const cursorArray = await cursor.toArray();
+
+      res.send(cursorArray);
+    });
+
+    app.post("/MyReview", async (req, res) => {
+      const email = req.body.email;
+      const emaili = { email };
+      console.log(emaili);
+      const query = { email: emaili.email };
+      const cursor = ReviewCollection.find(query);
+      const cursorArray = await cursor.toArray();
+      console.log(cursorArray);
+      res.send(cursorArray);
+    });
+
+    // app.post("/detailsId", async (req, res) => {
+    //   const data = req.body.data;
+    //   console.log(data);
+    //   const query = { _id: ObjectId(data) };
+
+    //   const search = await servicesCollection.findOne(query);
+
+    //   res.send(search);
+    // });
+
+    app.listen(port, () => {
+      console.log(" helix server running on port ", port);
+    });
+  } finally {
   }
-});
+}
 
-app.get("/details/:id", (req, res) => {
-  const data = req.params.id;
-
-  res.send({ data });
-});
-
-app.post("/detailsId", (req, res) => {
-  const data = req.body.data;
-  let search = ServiceData.find((serv) => serv.service_id === data);
-  console.log(search);
-  res.send(search);
-});
-
-app.listen(port, () => {
-  console.log(" helix server running on port ", port);
-});
+connection().catch((er) => console.error(er));
